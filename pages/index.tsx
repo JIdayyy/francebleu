70534c12d55fe6295a4a903113139be863a1de9e
JIdayyy <julien_abbadie@hotmail.fr>
2021-09-19 22:31:47 +0200

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
 
     return (
         <div className="w-full z-0 relative flex flex-col items-center align-middle justify-center h-screen">
-            <div className="absolute z-50 top-10 left-10">
+            <div className="absolute hidden sm:flex z-50 top-10 left-10">
                 <a href="https://www.lezardsquibougent.com/">
                     <Image
                         width={130}
@@ -22,11 +22,11 @@ const Home: React.FC = () => {
             <div className="z-0 relative w-full h-full">
                 <Image className="z-0" layout="fill" src="/bgalien.jpg" />
             </div>
-            <div className="absolute top-10 flex flex-col text-white font-bold bg-black rounded-lg bg-opacity-90 p-5">
+            <div className="absolute text-sm items-center align-middle justify-between top-0 sm:top-40 flex flex-col text-gray-200 font-bold bg-black rounded-lg bg-opacity-90 p-5">
                 {state.tracks[state.index].name}
                 <span className="font-normal">( {state.count} écoutes )</span>
             </div>
-            <div className="z-50 absolute text-white w-96 bg-black bg-opacity-90 rounded-lg p-10">
+            <div className="z-50 absolute text-white sm:w-96 bg-black bg-opacity-90 rounded-lg p-10">
                 Un feuilleton proposé par la Compagnie Lézards qui Bougent,
                 imaginé et écrit par les jeunes de l'Espace Socio-Culturel
                 Municipal et de la Maison de la Vie Citoyenne Saint-Étienne de
