690c16e8275231b660bf94bfa8dede0a5e7fbe22
JIdayyy <julien_abbadie@hotmail.fr>
2021-10-01 13:42:59 +0200

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import Image from "next/image";
 import { Playlist } from "@components/Playlist";
 import { Playbar } from "@components/Playbar";
